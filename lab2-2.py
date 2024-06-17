68ab2f012e74941b8088ae53ab6728b8855d1d29
millmai <6529011016@cdti.ac.th>
2024-06-17 14:04:40 +0700

    update

---
+++ lab2-2.py
@@ -0,0 +1,14 @@
+x = int(input("รับค่าคะแนน: "))
+print("คะแนน =",x)
+if x >= 80 and x <= 100:
+    print("เกรด A")
+elif x >= 70 and x <= 79:
+    print("เกรด B")
+elif x >= 60 and x <= 69:
+    print("เกรด C")
+elif x >= 50 and x <= 59:
+    print("เกรด D")
+elif x >= 0 and x <= 49:
+    print("เกรด F")
+else:
+    print("กรุณากรอกข้อมูล 0-100")
