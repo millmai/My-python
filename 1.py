c4d164dd5305f999622d56bd3169353b3c66f4aa
millmai <6529011016@cdti.ac.th>
2024-06-17 16:12:42 +0700

    update

---
+++ 1.py
@@ -0,0 +1,10 @@
+x = 10
+while x>0:
+    print(x)
+    x-=1
+    
+print("--------------")
+y = 0
+ehile y<10:
+    print(y)
+    y+=2
---
+++ 3.py
@@ -0,0 +1,6 @@
+x= int(input("Multiple:"))
+y=1
+while y<=12:
+    z=x*y
+    print("%d x %d = %d" %(x,y,z))
+    y+=1
