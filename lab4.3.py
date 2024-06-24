27ea5c4d7d2d5c05c3ffec5699e5e7f35ec50f31
millmai <6529011016@cdti.ac.th>
2024-06-24 14:43:43 +0700

    update

---
+++ lab4.3.py
@@ -0,0 +1,9 @@
+person = {"name": "Soraras", "age":17, "wt": 39, "h":162}
+#for n in person:
+#    print(n)
+print(person["name"])
+print(person["age"])
+print(person["wt"])
+print(person["h"])
+person["name"] = "Millmai"
+print(person["name"])
